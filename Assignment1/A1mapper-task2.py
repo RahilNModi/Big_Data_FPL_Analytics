c2635a7e442924939c957d243bf66958244e2c5b
soorya nath <56500311+SooryanathIT@users.noreply.github.com>
2020-09-02 16:24:01 +0530

    Update A1mapper-task2.py

--- Assignment1/A1mapper-task2.py
+++ Assignment1/A1mapper-task2.py
@@ -16,21 +16,23 @@ for recs in sys.stdin :	#read from input stream
 	
 	curr_word = pure_dict['word']	#get the word attr
 	
-	if curr_word == target:	#perform comparison
+	if curr_word == target and all(x.isalpha() or x.isspace() for x in pure_dict["word"]) and type(pure_dict["recognized"])==bool and all(len(y)==2 for y in pure_dict["drawing"]):	#perform comparison
 		
-		#print(pure_dict['countrycode'],'  ',1)
+		if len(pure_dict["countrycode"])==2 and pure_dict["countrycode"].isupper() and len(pure_dict["key_id"])==16 and pure_dict["key_id"].isnumeric():
 		
-		stroke_one = pure_dict['drawing'][0]	#get the first stroke from drawing
-		
-		stroke_one_x = stroke_one[0][0]	#x coord from stroke_one list
-		#print("Stroke_one_x:",stroke_one_x)
-		stroke_one_y = stroke_one[1][0]	#y coord from stroke_one list
-		#print("Stroke_one_y:",stroke_one_y)
-		#print(stroke_one_x,stroke_one_y,sep="  ,   ")
-		
-		if (stroke_one_x**2 + stroke_one_y**2 )**0.5 > threshold_distance :	#condition check , euclidean criteria
-		
-			print(pure_dict['countrycode'],',',1,sep="")		#print
+				#print(pure_dict['countrycode'],'  ',1)
+
+				stroke_one = pure_dict['drawing'][0]	#get the first stroke from drawing
+
+				stroke_one_x = stroke_one[0][0]	#x coord from stroke_one list
+				#print("Stroke_one_x:",stroke_one_x)
+				stroke_one_y = stroke_one[1][0]	#y coord from stroke_one list
+				#print("Stroke_one_y:",stroke_one_y)
+				#print(stroke_one_x,stroke_one_y,sep="  ,   ")
+
+				if (stroke_one_x**2 + stroke_one_y**2 )**0.5 > threshold_distance :	#condition check , euclidean criteria
+
+					print(pure_dict['countrycode'],',',1,sep="")		#print
 	
 	
 
