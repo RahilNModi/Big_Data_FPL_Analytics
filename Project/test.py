db9d68050662d290ea3200e5db686049ef17e2d2
soorya nath <56500311+SooryanathIT@users.noreply.github.com>
2020-11-20 16:39:57 +0530

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -1,17 +1,13 @@
-#import findspark
-
-#findspark.init()
-
 from pyspark import SparkConf,SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import Row,SQLContext
-#import pyspark_csv as pycsv 
+from pyspark.sql import Row,SQLContext 
 import sys
+import json
 
 configuration = SparkConf()
 configuration.setAppName('Project')
 
-spark_context = SparkContext(conf=configuration)
+spark_context = SparkContext('local[2]',conf=configuration)
 streaming_context = StreamingContext(spark_context,2)
 
 streaming_context.checkpoint('Project Checkpoint')
@@ -27,9 +23,9 @@ input_stream = streaming_context.socketTextStream('localhost',6100)
 
 sql = SQLContext(spark_context)
 
-players_df = sql.read.format('csv').option("header", "true").load("/home/soorya/Documents/project/BD_FPL/BD_FPL/Data/play.csv")
+players_df = sql.read.format('csv').option("header", "true").load("path/BD_FPL/BD_FPL/Data/play.csv")
 
-teams_df = sql.read.format('csv').option("header", "true").load("/home/soorya/Documents/project/BD_FPL/BD_FPL/Data/teams.csv")
+teams_df = sql.read.format('csv').option("header", "true").load("path/BD_FPL/BD_FPL/Data/teams.csv")
 
 players_df.show(10)
 
@@ -39,8 +35,7 @@ teams_df.show(10)
 ##3#######################################################
   
 	
-	     
-		 #WORK HERE#
+input_stream.pprint()
 
 			
 			
